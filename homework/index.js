d3a679a50341e7fd59e0c8d434826da6505652a1
Yusuf <ysf.csr@gmail.com>
2020-05-13 20:13:18 +0200

    first part of the project

--- homework/index.js
+++ homework/index.js
@@ -20,20 +20,22 @@
     const elem = document.createElement(name);
     parent.appendChild(elem);
     Object.entries(options).forEach(([key, value]) => {
-      if (key === 'text') {
-        elem.textContent = value;
+      if (key === 'Repository' || key === 'Description' || key === 'Forks' || key === 'Updated') {
+        elem.innerHTML += `<strong>${key}:</strong> ${value} <br>`;
       } else {
         elem.setAttribute(key, value);
       }
     });
     return elem;
   }
-
   function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul, { text: repo.name });
+    createAndAppend('li', ul, { Repository: repo.name, Description: repo.description, Forks: repo.forks, Updated: repo.updated_at });
   }
 
   function main(url) {
+    const heading = document.createElement('h1');
+    heading.innerText = 'HYF Repositories';
+    document.body.prepend(heading);
     fetchJSON(url, (err, repos) => {
       const root = document.getElementById('root');
       if (err) {
@@ -44,6 +46,8 @@
         return;
       }
       const ul = createAndAppend('ul', root);
+      repos.sort((a, b) => a.name.localeCompare(b.name, 'en', { sensitivity: 'base' }));
+      // repos.slice(0, 10);
       repos.forEach(repo => renderRepoDetails(repo, ul));
     });
   }
